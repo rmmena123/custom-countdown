e4c24b15bd13ab6f9977c7a830f60cf055a8c088
Rodrigo Machado Mena <rodrigo_mena_corebiz@whirlpool.com>
2022-11-11 20:06:33 -0300

    #5 - Pegar dados do formulário

--- script.js
+++ script.js
@@ -2,5 +2,16 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let coundownTitle = '';
+let coundownDate = '';
+
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+}
+
+countdownForm.addEventListener('submit', updateCountdown);
